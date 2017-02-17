c1c55b844571bd4750f36c11e4f8ded3604b23f6
administrador <administrador@maquina47.itchii.lab>
2017-02-17 10:08:28 -0700

    Practica 3, LOG IN

--- 1ER_EVA_PRACTICA_3/ViewController.swift
+++ 1ER_EVA_PRACTICA_3/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func Button(sender: AnyObject) {
+        var sID = lblID.text
+        var sPassword = lblPassword.text
+        if(sID == "Javier" && sPassword == "123456"){
+            let acMiDialogo = UIAlertController(title: "Bienvenido usuario", message: sID, preferredStyle: .Alert)
+            let Button = UIAlertAction(title: "Aceptar", style: .Cancel, handler: nil)
+            acMiDialogo.addAction(Button)
+            presentViewController(acMiDialogo, animated: true, completion: nil)
+        } else {
+            lblConfirma.text = "Error de usuario o password"
+                    }
+           }
+    
+    @IBAction func onTapGestureRecognizer(sender: AnyObject) {
+        lblID.resignFirstResponder()
+        lblPassword.resignFirstResponder()
+    }
+
+    
+    @IBOutlet weak var lblConfirma: UILabel!
+    @IBOutlet weak var lblPassword: UITextField!
+    @IBOutlet weak var lblID: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
